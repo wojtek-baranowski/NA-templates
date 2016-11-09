4101bb64f40ebe8f6cdf98a3524187f1f14bd7b2
wojtek-baranowski <wojciech.jan.baranowski@gmail.com>
2016-11-09 15:58:58 +0100

    21 container update

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -1,5 +1,30 @@
 #pragma once
 
+#include <cstddef>
+#include <utility>
+
 template<typename T>
-class container_wrapper;
+class container_wrapper
+{
+	private:
+		T _value;	
+
+	public: 
+
+		container_wrapper() = default;
+		container_wrapper (T arg): _value(std::move(arg))
+	{
+
+	} 
+		container_wrapper(const container_wrapper &) = default;
+		container_wrapper(container_wrapper &&) = default;
+		container_wrapper & operator=(const container_wrapper &) = default;
+		container_wrapper & operator=(container_wrapper &&) = default;
+
+		std::size_t size() const
+	{
+			return _value.size();
+	}
+
+};
 
