9484a6df533ec42b753c262b0945c8aa19bc03e2
wojtek-baranowski <wojciech.jan.baranowski@gmail.com>
2016-11-10 12:52:23 +0100

    Adding type checking mechanism

--- 30-traits/implementation.h
+++ 30-traits/implementation.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include<type_traits>
+
 template<typename T, typename T1, typename T2, typename T3>
-struct is_any_of;
+struct is_any_of  :  std::disjunction<std::is_same<T,T1>, std::is_same<T,T2>,
+std::is_same<T,T3>>
+{
+};
 
