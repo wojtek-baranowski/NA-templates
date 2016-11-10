446dc21567214af7c96496a172f5945ea0d456d3
wojtek-baranowski <wojciech.jan.baranowski@gmail.com>
2016-11-10 15:58:35 +0100

    function template exercise

--- 40-algos-impl/find_if.h
+++ 40-algos-impl/find_if.h
@@ -2,7 +2,15 @@
 
 namespace training
 {
-template<typename InputIt, typename UnaryPredicate>
-InputIt find_if(InputIt begin, InputIt end, UnaryPredicate pred);
+	template<typename InputIt, typename UnaryPredicate>
+		InputIt find_if(InputIt begin, InputIt end, UnaryPredicate pred)
+		{
+			for(; begin != end; ++begin)
+			{	if (pred(*begin) == true )
+				return begin;
+			}
+			return end;   
+		}
+
 }
 
--- 40-algos-impl/vector_erase.h
+++ 40-algos-impl/vector_erase.h
@@ -1,4 +1,10 @@
 #pragma once
-
+#include <vector>
+#include <algorithm>
 // define vector_erase here
 
+	template<typename T>
+	void vector_erase (T & vec, const typename  T::value_type & value)
+{
+	vec.erase (std::remove(vec.begin(),vec.end(), value), vec.end());
+}
