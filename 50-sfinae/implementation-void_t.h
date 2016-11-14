bcefa31a30c3cb5e6fdb41481d5c66feca34739b
wojtek-baranowski <wojciech.jan.baranowski@gmail.com>
2016-11-14 11:50:40 +0100

    SFINAE exercises

--- 50-sfinae/implementation-void_t.h
+++ 50-sfinae/implementation-void_t.h
@@ -1,17 +1,20 @@
 #pragma once
+#include <type_traits>
+template <typename... Ts> struct voider { using type = void; };
 
-template<typename... Ts>
-struct voider
-{
-    using type = void;
-};
+template <typename... Ts> using void_t = typename voider<Ts...>::type;
 
-template<typename... Ts>
-using void_t = typename voider<Ts...>::type;
-
-template<typename T>
+template <typename T, typename = void>
 struct has_type_typedef : std::false_type {};
 
-template<typename T>
+template <typename T>
+struct has_type_typedef<T, void_t<typename T::type>> : std::true_type {};
+
+template <typename T, typename = void>
 struct has_member_variable : std::false_type {};
 
+template <typename T>
+struct has_member_variable<
+    T, void_t<typename std::enable_if<
+           std::is_same<int, decltype(std::declval<T>().member)>::value>::type>>
+    : std::true_type {};
--- 50-sfinae/implementation.h
+++ 50-sfinae/implementation.h
@@ -1,8 +1,23 @@
 #pragma once
+#include <type_traits>
+template <typename T> struct has_type_typedef {
+private:
+  template <typename U>
+  static auto test(int) -> decltype(std::declval<typename U::type>(), void());
+  //      template <typename U, typename = typename U::type>
+  //	static void test(int); "also you can that way"
+  template <typename U> static char test(long);
 
-template<typename T>
-struct has_type_typedef;
+public:
+  static constexpr bool value = std::is_void<decltype(test<T>(0))>::value;
+};
 
-template<typename T>
-struct has_member_variable;
+template <typename T> struct has_member_variable {
+private:
+  template <typename U>
+  static auto test(int) -> decltype(std::declval<U>().member, void());
+  template <typename U> static char test(long);
 
+public:
+  static constexpr bool value = std::is_void<decltype(test<T>(0))>::value;
+};
